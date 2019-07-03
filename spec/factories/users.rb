b74f7153704004b8d5693e6b8bed1a6f0d1ca6c8
federicomoyamartin <federicomoyamartin@gmail.com>
2019-07-03 17:01:58 -0300

    API-25 Set up testing suite

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  first_name :string
+#  last_name  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+  end
+end

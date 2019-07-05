7d19d44d0327343111bf315fa6f54b8fc1c3b549
Federico Moyá <federicomoyamartin@gmail.com>
2019-07-05 16:46:00 -0300

    API-25 Set up testing suite (#10)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20_190_703_155_941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'first_name'
+    t.string 'last_name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  first_name :string
+#  last_name  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+RSpec.describe User, type: :model do
+  it 'has a valid factory' do
+    expect(build(:user)).to be_valid
+  end
+end

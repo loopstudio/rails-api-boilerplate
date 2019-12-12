6c973f9b2a3c895ac9879ceb8d13ba5708993caa
lucaleivaloop <lleiva@loopstud.io>
2019-12-12 11:15:38 -0300

    Add reviewer suggestions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: ENV.fetch('HOST') }
+  config.action_mailer.default_url_options = { host: ENV['SERVER_URL'] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  domain: ENV.fetch('DOMAIN'),
+  domain: ENV['MAILER_DOMAIN'],
   address: 'smtp.sendgrid.net',
   port: 465,
   authentication: :plain,
   user_name: 'apikey',
-  password: ENV.fetch('SENDGRID_API_KEY'),
+  password: ENV['SENDGRID_API_KEY'],
   enable_starttls_auto: true
 }

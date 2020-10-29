d30004021e5e576cf951fad48e2d217bb5d1e8d5
Micaela Wolman <mwolman@loopstudio.dev>
2020-10-29 12:16:19 -0300

    Update spec/requests/api/v1/passwords/update_spec.rb file

--- spec/requests/api/v1/passwords/update_spec.rb
+++ spec/requests/api/v1/passwords/update_spec.rb
@@ -1,115 +1,67 @@
 require 'rails_helper'
 require 'addressable/uri'
 
-describe 'PUT api/v1/users/password/', type: :request do
-  subject(:put_request) do
-    put user_password_path, params: params, headers: headers, as: :json
-  end
+describe 'PUT api/v1/users/password/', { type: :request } do
+  subject(:put_request) { put user_password_path, params: params, headers: headers, as: :json }
 
   let(:user) { create(:user) }
-
   let!(:password_token) { user.send(:set_reset_password_token) }
   let(:new_password) { '123456789aA?!' }
+  let(:params) do
+    {
+      reset_password_token: password_token,
+      password: new_password
+    }
+  end
+
+  before { |example| put_request unless example.metadata[:skip_before] }
 
   context 'with a valid token' do
-    let(:params) do
-      {
-        reset_password_token: password_token,
-        password: new_password
-      }
-    end
-
     context 'with valid params' do
-      it 'returns a successful response' do
-        put_request
-
-        expect(response).to be_successful
-      end
-
-      it 'updates the user password' do
+      it 'updates the user password', skip_before: true do
         expect {
           put_request
         }.to(change { user.reload.encrypted_password })
       end
 
-      it 'makes the new password valid' do
-        put_request
-
-        expect(user.reload.valid_password?(new_password)).to eq(true)
-      end
+      it { expect(response).to be_successful }
+      it { expect(user.reload).to be_valid_password(new_password) }
 
       it 'returns the user data' do
-        put_request
-
-        expect(json[:user]).to include_json(
-          id: user.id,
-          first_name: user.first_name,
-          last_name: user.last_name,
-          email: user.email
-        )
+        expect(json[:user]).to include_json(id: user.id,
+                                            first_name: user.first_name,
+                                            last_name: user.last_name,
+                                            email: user.email)
       end
 
       it 'returns a valid client and access token' do
-        put_request
-
         token = response.header['access-token']
         client = response.header['client']
+
         expect(user.reload).to be_valid_token(token, client)
       end
     end
 
     context 'with invalid params' do
-      let(:params) do
-        {
-          reset_password_token: password_token,
-          password: nil
-        }
-      end
+      let(:new_password) { nil }
 
-      specify do
-        put_request
+      it { expect(response).to have_http_status(:bad_request) }
+      it { expect(json[:attributes_errors]).not_to have_key('reset_password_token') }
 
-        expect(response).to have_http_status(:bad_request)
-      end
-
-      specify do
-        put_request
-
-        expect(json[:attributes_errors]).not_to have_key('reset_password_token')
-      end
-
-      it 'does not change the user password' do
-        expect {
-          put_request
-        }.not_to change(user.reload, :encrypted_password)
+      it 'does not change the user password', skip_before: true do
+        expect { put_request }.not_to change(user.reload, :encrypted_password)
       end
     end
   end
 
   context 'with an invalid token' do
-    let(:params) do
-      {
-        reset_password_token: 'invalid token',
-        password: new_password
-      }
-    end
+    let(:password_token) { 'invalid token' }
 
-    specify do
-      put_request
+    it { expect(response).to have_http_status(:bad_request) }
+    it { expect(user.reload).not_to be_valid_password(new_password) }
 
-      expect(response).to have_http_status(:bad_request)
-    end
-
-    it 'does not change the user password' do
-      expect {
-        put_request
-      }.not_to change(user.reload, :encrypted_password)
-    end
-
-    it 'does not make the new password valid' do
-      put_request
-
-      expect(user.reload).not_to be_valid_password(new_password)
+    it 'does not change the user password', skip_before: true do
+      expect { put_request }.not_to change(user.reload, :encrypted_password)
     end
   end
 end

d1f78e0c5e35086740a46f7407f06c521afbe728
Jaime García Méndez <jgarcia@loopstudio.dev>
2021-06-22 13:05:31 -0300

    Add Parameter Filtering in Sentry and define enabled environments

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,14 @@
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-  config.environment = Rails.env
+  config.enabled_environments = %w[production staging] # Specify enabled environments
   config.send_default_pii = true
   config.sample_rate = 1.0
   config.async = ->(event) { Sentry::SendEventJob.perform_later(event) }
+
+  # Param filtering
+  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+  config.before_send = lambda do |event, _hint|
+    filter.filter(event.to_hash)
+  end
 end

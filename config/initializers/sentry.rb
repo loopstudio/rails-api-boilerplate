3a248aaac03388ee2d88fbda75f0096929b069df
Jaime García Méndez <jgarcia@loopstudio.dev>
2021-06-22 11:49:18 -0300

    Remove fetch from Sentry DSN env var

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = ENV.fetch('SENTRY_DSN')
+  config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.environment = Rails.env
   config.send_default_pii = true

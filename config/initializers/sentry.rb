a29ab020589523d2741e633e96e026851305d370
Augusto Studer <elestu@users.noreply.github.com>
2021-06-22 15:01:03 -0300

    Update config/initializers/sentry.rb

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,6 @@
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-  config.enabled_environments = %w[production staging] # Specify enabled environments
   config.send_default_pii = true
   config.sample_rate = 1.0
   config.async = ->(event) { Sentry::SendEventJob.perform_later(event) }

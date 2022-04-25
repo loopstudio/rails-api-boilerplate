9e0749aca30c82f1ced70e3df3fd4bacf7e77865
Augusto Studer <elestu@users.noreply.github.com>
2022-04-25 10:58:43 -0300

    chore: Fix deprecations for Rails config & RackAttack (#626)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations.
-  config.action_view.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
 
   config.after_initialize do
     Bullet.enable = true
